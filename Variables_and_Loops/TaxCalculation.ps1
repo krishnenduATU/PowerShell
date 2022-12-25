db22172f8b51a38e65a1facca784fa2832a8a01f
krishnenduvp <l00170964@atu.ie>
2022-12-25 11:24:14 +0530

    BugFix #1 : Updated VAT variable value

--- Variables_and_Loops/TaxCalculation.ps1
+++ Variables_and_Loops/TaxCalculation.ps1
@@ -1,6 +1,6 @@
 ﻿#Tax calculation
 $amount = 300
-$VAT = 0.30
+$VAT = 0.23
 $result = $amount * $VAT
 $result_withtax = $result + $amount
 $text = "Total €$result_withtax is the sum of €$amount with 23% VAT"
