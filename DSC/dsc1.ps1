fdec7757eb1be88043f00ad29b1ca66c0e3ee3a0
krishnenduvp <l00170964@atu.ie>
2022-12-25 23:35:08 +0530

    PowerShell DSC Scripts

---
+++ DSC/dsc1.ps1
@@ -0,0 +1,32 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+Configuration DscConfiguration
+{
+
+    param
+    (
+        [string[]]$ComputerName='localhost'
+    )
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    Node $ComputerName
+    {
+        WindowsFeature MyFeatureInstance
+        {
+            Ensure = 'Present'
+            Name = 'RSAT'
+        }
+
+        WindowsFeature My2ndFeatureInstance
+        {
+            Ensure = 'Present'
+            Name = 'DNS'
+        }
+        File HelloWorld {
+            DestinationPath = "C:\Temp\HelloWorld.txt"
+            Ensure = "Present"
+            Contents   = "Hello World from DSC!"
+        }
+    }
+}
+DscConfiguration
