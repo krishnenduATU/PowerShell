9b703c185792ef8cb20c378d9564d82d79dea393
krishnenduvp <l00170964@atu.ie>
2022-12-27 08:25:16 +0530

    BugFix #8 : Added comments to code

--- Variables_and_Loops/do_until.ps1
+++ Variables_and_Loops/do_until.ps1
@@ -1,8 +1,15 @@
-﻿$a = 0
+﻿<# 
+Do-until loop
+This program will execute until the value of variable "a" reaches 5
+#>
+# Value of "a" is set to 0
+$a = 0
 do
 {
 “Starting Loop $a”
 $a
+# To increment the value of "a" by one
 $a++
 “Now vairable `$a is $a”
-} until ($a -ge 5)
+# until( test_expression) Loop exists when value of a is greater than or equal to 5 
+} until ($a -ge 5) 
